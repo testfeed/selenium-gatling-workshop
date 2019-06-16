70aa293b92d77b1fe9693ea5e2acf1f947814ff0
testfeed <testfeed@users.noreply.github.com>
2019-06-16 13:54:54 +0100

    add more context to stage-1 to help with building out the scenarios

--- gatling/src/test/scala/com/testfeed/CheckoutSimulation.scala
+++ gatling/src/test/scala/com/testfeed/CheckoutSimulation.scala
@@ -1,33 +1,18 @@
 package com.testfeed
 
-import com.testfeed.ScenarioBuilder._
 import com.testfeed.config.SimulationConfig
+import com.testfeed.steps.{LoginSteps, RegistrationSteps}
 import io.gatling.core.Predef._
-import io.gatling.core.structure.PopulationBuilder
 import io.gatling.http.Predef.http
 
 class CheckoutSimulation extends Simulation
-  with SimulationConfig {
+  with SimulationConfig
+  with LoginSteps
+  with RegistrationSteps {
 
-  private def withInjectedLoad(): Seq[PopulationBuilder] = {
+  val mainScn = scenario("Register and login")
 
-    val injectionSteps = List(
-      rampUsersPerSec(minUsers).to(maxUsers).during(rampUpTime),
-      constantUsersPerSec(maxUsers).during(constantRateTime),
-      rampUsersPerSec(maxUsers).to(minUsers).during(rampDownTime)
-    )
-    Seq(checkoutScenario("checkout juice").inject(injectionSteps))
-  }
-
-  if (runSingleUserJourney) {
-    val injectedBuilders = Seq(checkoutScenario("checkout juice").inject(atOnceUsers(1)))
-
-    setUp(injectedBuilders: _*).protocols(http)
-      .assertions(global.failedRequests.count.is(0))
-
-  } else {
-
-    setUp(withInjectedLoad(): _*).protocols(http)
-      .assertions(global.failedRequests.percent.lt(1))
-  }
+  setUp(
+    mainScn.inject(atOnceUsers(1))
+  ).protocols(http.baseUrl(baseUrl))
 }
--- gatling/src/test/scala/com/testfeed/steps/RegistrationSteps.scala
+++ gatling/src/test/scala/com/testfeed/steps/RegistrationSteps.scala
@@ -1,6 +1,5 @@
 package com.testfeed.steps
 
-import com.testfeed.ScenarioBuilder._
 import io.gatling.core.Predef._
 import io.gatling.http.Predef._
 
@@ -8,49 +7,16 @@ import scala.util.Random
 
 trait RegistrationSteps {
 
-  val registrationUrl: String = s"$baseUrl/api/Users/"
-  val securityQuestionsUrl: String = s"$baseUrl/api/SecurityQuestions/"
-  val securityAnswersUrl: String = s"$baseUrl/api/SecurityAnswers/"
+  val registrationPath: String = "/api/Users/"
+  val securityQuestionsUrl: String = "api/SecurityQuestions/"
+  val securityAnswersUrl: String = "/api/SecurityAnswers/"
   val userIdPattern = """"id":([\d]+),"""
 
   val emailFeeder = Iterator.continually(Map("email" -> s"${Random.alphanumeric.take(10).mkString}@testing.com"))
   val passwordFeeder = Iterator.continually(Map("password" -> s"${Random.alphanumeric.take(15).mkString}"))
 
-  val getSecurityQuestions = http("Get security questions")
-    .get(securityQuestionsUrl)
-    .check(status lt 400)
+  val registrationRequest = http("Registration")
+    .post(registrationPath)
+    .body(StringBody("test registration"))
 
-  val register = http("Registration")
-    .post(registrationUrl)
-    .header("Content-Type", "application/json")
-    .body(StringBody(
-      """
-        |{
-        |  "email": "${email}",
-        |  "password": "${password}",
-        |  "passwordRepeat": "${password}",
-        |  "securityQuestion": {
-        |    "id": 2,
-        |    "question": "Mother's maiden name?",
-        |    "createdAt": "2019-06-03T19:54:37.113Z",
-        |    "updatedAt": "2019-06-03T19:54:37.113Z"
-        |  },
-        |  "securityAnswer": "oo4PGZpyzR"
-        |}
-      """.stripMargin))
-    .check(regex(userIdPattern).saveAs("userId"))
-    .check(status lt 400)
-
-  val storeSecurityAnswer = http("Send security answer")
-    .post(securityAnswersUrl)
-    .header("Content-Type", "application/json")
-    .body(StringBody(
-      """
-        |{
-        |  "UserId": ${userId},
-        |  "answer": "mmn",
-        |  "SecurityQuestionId": 2
-        |}
-      """.stripMargin))
-    .check(status lt 400)
 }
